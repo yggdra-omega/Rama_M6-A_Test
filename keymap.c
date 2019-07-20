adba2b13ed1ce285d7334bd18c0ad1d109ecbf0f
yggdra-omega <48136860+yggdra-omega@users.noreply.github.com>
2019-07-20 15:25:58 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -2,19 +2,33 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =	{
 
-    LAYOUT(
-        TO(1), KC_1, KC_1, KC_1, KC_1, KC_1),
-
-    LAYOUT(
-        TO(2),  KC_2, KC_2, KC_2, KC_2, KC_2),
-
-    LAYOUT(
-        TO(3),  KC_3, KC_3, KC_3, KC_3, KC_3),
-
 	LAYOUT(
-        TO(4),  KC_4, KC_4, KC_4, KC_4, KC_4),
-
-    LAYOUT(
-        TO(0),  KC_5, KC_5, KC_5, KC_5, KC_5)
-		
-																};
+		TO(1),	KC_1,	KC_1,	\
+		KC_1,	KC_1,	KC_1	\
+		),
+	
+	LAYOUT(
+		TO(2),	KC_2,	KC_2,	\
+		KC_2,	KC_2,	KC_2	\
+		),
+	
+	LAYOUT(
+		TO(3),	KC_3,	KC_3,	\
+		KC_3,	KC_3,	KC_3	\
+		),
+	
+	LAYOUT(
+		TO(4),	KC_4,	KC_4,	\
+		KC_4,	KC_4,	KC_4	\
+		),
+	
+	LAYOUT(
+		TO(5),	KC_5,	KC_5,	\
+		KC_5,	KC_5,	KC_5	\
+		),
+	
+	LAYOUT(
+		TO(0),	KC_6,	KC_6,	\
+		KC_6,	KC_6,	KC_6	\
+		)
+								};
